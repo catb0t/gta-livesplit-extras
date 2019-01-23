284019ffb0b031d8fd8f74a5f227dfc73572b8a0
Cat Stevens <catb0t@protonmail.ch>
2019-01-23 12:36:29 -0500

    Add files via upload

---
+++ livesplit.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name     LiveSplit GTA Font+Key
+// @author	 catb0t
+// @version  0.1
+// @grant    none
+// ==/UserScript==
+
+console.log("Font+Key init") ;
+document.getElementsByTagName("body")[0].style["background-color"] = "#00ff00";
+var checkExist = setInterval(function() {
+	let T = document.getElementsByClassName("timer-time"); 	 
+  if (T.length) {
+		console.log(T);
+
+    let gradient = document.getElementById("Timer0-text-gradient")
+    gradient.innerHTML = `
+    <stop offset="0%" style="stop-color: rgb(255, 255, 255);"></stop>
+    <stop offset="100%" style="stop-color: #777;"></stop>`; // #898989, #808080
+    gradient.parentNode.insertAdjacentHTML('beforeend', 
+    `<filter id="Timer0_filter_shadow_offset" x="0" y="0" width="200%" height="200%">
+      <feOffset result="offOut" in="SourceAlpha" dx="2" dy="2" />
+      <feBlend in="SourceGraphic" in2="offOut" mode="normal" />
+    </filter>`);
+
+    for (var i = 0; i < T.length; i++) { 
+      console.log( T.item(i).style['font-family'] = "pricedown, sans-serif" );
+      console.log( T.item(i).style['stroke'] = "#000" );
+      console.log( T.item(i).style['stroke-width'] = "1" );
+      console.log( T.item(i).setAttribute("filter", "url(#Timer0_filter_shadow_offset)") );
+    }
+    
+		clearInterval(checkExist);
+  }
+}, 100);
+
+/*
+body background: hsl(120.5, 100%, 50%)
+  /html/body
+font-family: timer->pricedown 
+  /html/body/div/div/div[1]/div[3]/div/div[1]/div/svg/text[1]
+  /html/body/div/div/div[1]/div[3]/div/div[1]/div/svg/text[2]
+
+import layout button: 
+  /html/body/div/div/div[1]/div[1]/div/button[9]
+  this.props.callbacks.importLayout()
+  
+import splits button: 
+  /html/body/div/div/div[1]/div[1]/div/button[3]
+  this.props.callbacks.importSplits()
+*/
+
